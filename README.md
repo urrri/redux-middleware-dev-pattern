7ca756d31f9d0e1eb53e0f046c4577dbbe9de228
Urrri <yury.go@gmail.com>
2019-03-08 22:08:28 +0200

    react + vue

---
+++ README.md
@@ -0,0 +1,13 @@
+# redux-middleware-dev-pattern
+
+## Project setup
+```
+yarn
+```
+
+### Compiles for development (multiple projects)
+```
+npm run vue
+npm run react
+npm run angular
+```
---
+++ packages/x-vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        <HelloWorld msg="Welcome to Your Vue.js App"/>
+    </div>
+</template>
+
+<script>
+  import HelloWorld from './components/HelloWorld.vue';
+  import { store } from '@project/x-redux';
+  import { connect } from 'redux-vuex';
+  import Vue from 'vue';
+               console.log(store.getState())
+  connect({
+    Vue,
+    store,
+  });
+
+  export default {
+    name: 'app',
+    components: {
+      HelloWorld,
+    },
+  };
+</script>
+
+<style>
+    #app {
+        font-family: 'Avenir', Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
+</style>
